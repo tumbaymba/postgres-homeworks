b84b4a6a7adade1b09d335166d6244ed60cb6231
Dmitry Pavlushin <pavlushindv@gmail.com>
2024-02-06 20:38:24 +0300

    Homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+employee_id int PRIMARY KEY,
+ first_name varchar(25) NOT NULL,
+ last_name varchar(25) NOT NULL,
+ title varchar(30) NOT NULL,
+ birth_date date,
+ notes text
+);
+
+CREATE TABLE customers (
+customer_id char(5) PRIMARY KEY,
+ company_name varchar(50) NOT NULL,
+ contact_name varchar(50) NOT NULL
+);
+
+CREATE TABLE orders (
+order_id int PRIMARY KEY,
+ customer_id varchar(5) REFERENCES customers(customer_id) NOT NULL,
+ employee_id int REFERENCES employees(employee_id) NOT NULL,
+ order_date date NOT NULL,
+ ship_city varchar(50) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,42 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+file_emp = 'north_data/employees_data.csv'
+file_cust = 'north_data/customers_data.csv'
+file_ord = 'north_data/orders_data.csv'
+
+emp = []
+cust = []
+ord = []
+
+with open(file_ord, newline='') as orders_file:
+    reader = csv.DictReader(orders_file)
+    for row in reader:
+        row_list_orders = list(row.values())
+        ord.append(row_list_orders)
+
+with open(file_emp, newline='') as csv_file:
+    reader = csv.DictReader(csv_file)
+    for row in reader:
+        row_list = list(row.values())
+        emp.append(row_list)
+
+with open(file_cust, newline='') as cust_file:
+    reader = csv.DictReader(cust_file)
+    for row in reader:
+        row_list_cust = list(row.values())
+        cust.append(row_list_cust)
+
+conn = psycopg2.connect(host="localhost", dbname="north", user="postgres", password="Rhts1428")
+
+cur = conn.cursor()
+cur.executemany("INSERT INTO employees VALUES(%s,%s,%s,%s,%s,%s)", emp)
+cur.executemany("INSERT INTO customers VALUES(%s,%s,%s)", cust)
+cur.executemany("INSERT INTO orders VALUES(%s,%s,%s,%s,%s)", ord)
+conn.commit()
+
+cur.close()
+conn.close()
+
+
